642c91d84070f8aa95743f04ad7227185bdddedf
John030903 <nguyenhoangkhanhduy030903@gmail.com>
2023-06-27 22:16:16 +0700

    Change to WSGI server

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    # serve(app, host='0.0.0.0', port=5000)
-    app.run(debug=True,host='0.0.0.0',port='5000')
+    serve(app, host='0.0.0.0', port=5000)
+    # app.run(debug=True,host='0.0.0.0',port='5000')
